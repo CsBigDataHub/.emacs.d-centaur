40fd77e85e8a7f522944f4161bb3eddb269f21e1
Chetan Koneru <chetan.koneru@optum.com>
2020-03-10 19:50:59 -0500

    Terraform lsp added to init-lsp.el file

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -83,7 +83,7 @@
      ;; configure terraform for lsp
      (add-to-list 'lsp-language-id-configuration '(terraform-mode . "terraform"))
      (lsp-register-client
-      (make-lsp-client :new-connection (lsp-stdio-connection '("~/.emacs.d/custom-el-scripts/lsp/terraform-lsp/terraform-lsp" "-enable-log-file"))
+      (make-lsp-client :new-connection (lsp-stdio-connection '("~/.emacs.d/lsp/terraform-lsp" "-enable-log-file"))
                        :major-modes '(terraform-mode)
                        :server-id 'terraform-ls))
      ;; my-personal
--- lisp/init-terraform.el
+++ lisp/init-terraform.el
@@ -10,14 +10,15 @@
   :after company
   :hook (terraform-mode . company-terraform-init))
 
-;; TODO -look in to it lat
-(add-to-list 'lsp-language-id-configuration '(terraform-mode . "terraform"))
+;; included in lsp-mode file
+;;(add-to-list 'lsp-language-id-configuration '(terraform-mode . "terraform"))
 
+;; included in lsp-mode file
 ;; TODO  get Terraform lsp from -- https://github.com/juliosueiras/terraform-lsp
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection '("~/.emacs.d/lsp/terraform-lsp" "-enable-log-file"))
-                  :major-modes '(terraform-mode)
-                  :server-id 'terraform-ls))
+;;(lsp-register-client
+;; (make-lsp-client :new-connection (lsp-stdio-connection '("~/.emacs.d/lsp/terraform-lsp" "-enable-log-file"))
+;;                  :major-modes '(terraform-mode)
+;;                  :server-id 'terraform-ls))
 
 (add-hook 'terraform-mode-hook #'lsp)
 
