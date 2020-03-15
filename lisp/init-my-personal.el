29d59bb7ac9dfe56f14336379638196a61d12a21
Chetan Koneru <chetan.koneru@optum.com>
2020-03-15 08:40:03 -0500

    Added pdf-tools

--- lisp/init-my-personal.el
+++ lisp/init-my-personal.el
@@ -355,4 +355,103 @@ happens within a region if one is selected."
 
 (use-package transpose-mark)
 
+;; my-personal-config
+
+;; Added this to build pdf tools
+(setenv "PKG_CONFIG_PATH" "/usr/local/lib/pkgconfig:/usr/local/Cellar/libffi/3.2.1/lib/pkgconfig")
+
+(use-package pdf-tools
+  :config
+  (pdf-tools-install)
+  (require 'pdf-tools)
+  (setq-default pdf-view-display-size 'fit-page)
+  (bind-keys :map pdf-view-mode-map
+    ("\\" . hydra-pdftools/body)
+    ("<s-spc>" .  pdf-view-scroll-down-or-next-page)
+    ("g"  . pdf-view-first-page)
+    ("G"  . pdf-view-last-page)
+    ("l"  . image-forward-hscroll)
+    ("h"  . image-backward-hscroll)
+    ("j"  . pdf-view-next-page)
+    ("k"  . pdf-view-previous-page)
+    ("e"  . pdf-view-goto-page)
+    ("u"  . pdf-view-revert-buffer)
+    ("al" . pdf-annot-list-annotations)
+    ("ad" . pdf-annot-delete)
+    ("aa" . pdf-annot-attachment-dired)
+    ("am" . pdf-annot-add-markup-annotation)
+    ("at" . pdf-annot-add-text-annotation)
+    ("y"  . pdf-view-kill-ring-save)
+    ("i"  . pdf-misc-display-metadata)
+    ("s"  . pdf-occur)
+    ("b"  . pdf-view-set-slice-from-bounding-box)
+    ("r"  . pdf-view-reset-slice))
+  ;; TODO - wait until it gets published in melpa
+  ;;(use-package org-pdftools
+  ;;:config
+    ;;;;(setq org-pdftools-root-dir /path/you/store/pdfs)
+  ;;(with-eval-after-load 'org
+  ;;(org-link-set-parameters "pdftools"
+  ;;:follow #'org-pdftools-open
+  ;;:complete #'org-pdftools-complete-link
+  ;;:store #'org-pdftools-store-link
+  ;;:export #'org-pdftools-export)
+  ;;(add-hook 'org-store-link-functions 'org-pdftools-store-link)))
+  ;;
+  ;;(use-package org-noter-pdftools
+  ;;:after (org-noter))
+  )
+
+(defhydra hydra-pdftools (:color blue :hint nil)
+  "
+                                                                      ╭───────────┐
+       Move  History   Scale/Fit     Annotations  Search/Link    Do   │ PDF Tools │
+   ╭──────────────────────────────────────────────────────────────────┴───────────╯
+         ^^_g_^^      _B_    ^↧^    _+_    ^ ^     [_al_] list    [_s_] search    [_u_] revert buffer
+         ^^^↑^^^      ^↑^    _H_    ^↑^  ↦ _W_ ↤   [_am_] markup  [_o_] outline   [_i_] info
+         ^^_p_^^      ^ ^    ^↥^    _0_    ^ ^     [_at_] text    [_F_] link      [_d_] dark mode
+         ^^^↑^^^      ^↓^  ╭─^─^─┐  ^↓^  ╭─^ ^─┐   [_ad_] delete  [_f_] search link
+    _h_ ←pag_e_→ _l_  _N_  │ _P_ │  _-_    _b_     [_aa_] dired
+         ^^^↓^^^      ^ ^  ╰─^─^─╯  ^ ^  ╰─^ ^─╯   [_y_]  yank
+         ^^_n_^^      ^ ^  _r_eset slice box
+         ^^^↓^^^
+         ^^_G_^^
+   --------------------------------------------------------------------------------
+        "
+  ("\\" hydra-master/body "back")
+  ("<ESC>" nil "quit")
+  ("al" pdf-annot-list-annotations)
+  ("ad" pdf-annot-delete)
+  ("aa" pdf-annot-attachment-dired)
+  ("am" pdf-annot-add-markup-annotation)
+  ("at" pdf-annot-add-text-annotation)
+  ("y"  pdf-view-kill-ring-save)
+  ("+" pdf-view-enlarge :color red)
+  ("-" pdf-view-shrink :color red)
+  ("0" pdf-view-scale-reset)
+  ("H" pdf-view-fit-height-to-window)
+  ("W" pdf-view-fit-width-to-window)
+  ("P" pdf-view-fit-page-to-window)
+  ("n" pdf-view-next-page-command :color red)
+  ("p" pdf-view-previous-page-command :color red)
+  ("d" pdf-view-dark-minor-mode)
+  ("b" pdf-view-set-slice-from-bounding-box)
+  ("r" pdf-view-reset-slice)
+  ("g" pdf-view-first-page)
+  ("G" pdf-view-last-page)
+  ("e" pdf-view-goto-page)
+  ("o" pdf-outline)
+  ("s" pdf-occur)
+  ("i" pdf-misc-display-metadata)
+  ("u" pdf-view-revert-buffer)
+  ("F" pdf-links-action-perfom)
+  ("f" pdf-links-isearch-link)
+  ("B" pdf-history-backward :color red)
+  ("N" pdf-history-forward :color red)
+  ("l" image-forward-hscroll :color red)
+  ("h" image-backward-hscroll :color red))
+
+(setq pdf-view-use-scaling t)
+;;my-personal-config
+
 (provide 'init-my-personal)
