34e817ee8c65ac9da34b0ebfd0ba1d1744f747d2
Chetan Koneru <chetan.koneru@optum.com>
2020-03-28 09:14:39 -0500

    committed dimmer

--- lisp/init-my-personal.el
+++ lisp/init-my-personal.el
@@ -357,11 +357,11 @@ happens within a region if one is selected."
 
 ;; my-personal-config
 
-(use-package dimmer
-  :custom
-  (dimmer-fraction 0.2)
-  :init
-  (dimmer-mode t))
+;; (use-package dimmer
+;;   :custom
+;;   (dimmer-fraction 0.2)
+;;   :init
+;;   (dimmer-mode t))
 
 ;; Added this to build pdf tools
 (setenv "PKG_CONFIG_PATH" "/usr/local/Cellar/libffi/3.2.1/lib/pkgconfig:/usr/local/Cellar/zlib/1.2.11/lib/pkgconfig:/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig")
