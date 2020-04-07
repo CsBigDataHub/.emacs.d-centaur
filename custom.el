6b3c1e3091c863105cf9d3f3baea18ca22c32867
Chetan Koneru <chetan.koneru@optum.com>
2020-04-07 18:26:00 -0500

    Added few custom settings

--- custom.el
+++ custom.el
@@ -67,7 +67,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(centaur-package-archives (quote melpa))
+ '(centaur-theme (quote default))
+ '(doom-themes-treemacs-theme "doom-colors")
+ '(magit-todos-keyword-suffix "\\(?:([^)]+)\\)?:"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
