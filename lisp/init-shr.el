e99adcd06e155b14e133215ab15b9f202cf184f7
Chetan Koneru <chetan.koneru@optum.com>
2020-04-28 10:12:19 -0500

    Updated shrface with new settings.

--- lisp/init-shr.el
+++ lisp/init-shr.el
@@ -3,8 +3,35 @@
   :after shr
   :straight (shrface :type git :host github :repo "chenyanming/shrface")
   :config
+  (shrface-basic)
   (setq nov-shr-rendering-functions shr-external-rendering-functions)
   (setq shrface-bullets-bullet-list '("☯" "☢" "❀" "◉" "⚫" "○" "✸" "✿" "~"))
+  (setq shrface-item-bullet "➤")
+  ;; eww support
+  (with-eval-after-load 'eww
+    (add-hook 'eww-after-render-hook 'shrface-mode))
+
+  ;; nov support
+  (with-eval-after-load 'nov
+    (setq nov-shr-rendering-functions '((img . nov-render-img) (title . nov-render-title))) ; reset nov-shr-rendering-functions, in case of the list get bigger and bigger
+    (setq nov-shr-rendering-functions (append nov-shr-rendering-functions shr-external-rendering-functions))
+    (add-hook 'nov-mode-hook 'shrface-mode))
+
+  ;; mu4e support
+  (with-eval-after-load 'mu4e
+    (add-hook 'mu4e-view-mode-hook 'shrface-mode))
+
+  (with-eval-after-load 'nov
+    (define-key nov-mode-map (kbd "<tab>") 'org-cycle)
+    (define-key nov-mode-map (kbd "S-<tab>") 'org-shifttab)
+    (define-key nov-mode-map (kbd "C-j") 'outline-next-visible-heading)
+    (define-key nov-mode-map (kbd "C-k") 'outline-previous-visible-heading))
+
+  (with-eval-after-load 'eww
+    (define-key eww-mode-map (kbd "<tab>") 'org-cycle)
+    (define-key eww-mode-map (kbd "S-<tab>") 'org-shifttab)
+    (define-key eww-mode-map (kbd "C-j") 'outline-next-visible-heading)
+    (define-key eww-mode-map (kbd "C-k") 'outline-previous-visible-heading))
   )
 
 
