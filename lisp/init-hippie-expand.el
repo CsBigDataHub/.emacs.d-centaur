5dda35934a0be7cffa4667c27f838b24f7b4caa5
Chetan Koneru <chetan.koneru@optum.com>
2020-11-09 11:17:16 -0600

    disabled company-ispell due to performance issues

--- lisp/init-hippie-expand.el
+++ lisp/init-hippie-expand.el
@@ -218,24 +218,24 @@
 ;; (add-hook 'markdown-mode-hook (lambda () (my-generic-ispell-company-complete-setup)))
 
 ;; https://emacs.stackexchange.com/questions/42508/how-to-use-an-ispell-dictionary-in-company-mode/42526#42526
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+;; (require 'company)
+;; (add-hook 'after-init-hook 'global-company-mode)
 
-(defun text-mode-hook-setup ()
-  ;; make `company-backends' local is critcal
-  ;; or else, you will have completion in every major mode, that's very annoying!
-  (make-local-variable 'company-backends)
+;; (defun text-mode-hook-setup ()
+;;   ;; make `company-backends' local is critcal
+;;   ;; or else, you will have completion in every major mode, that's very annoying!
+;;   (make-local-variable 'company-backends)
 
-  ;; company-ispell is the plugin to complete words
-  (add-to-list 'company-backends 'company-ispell)
+;;   ;; company-ispell is the plugin to complete words
+;;   (add-to-list 'company-backends 'company-ispell)
 
-  ;; OPTIONAL, if `company-ispell-dictionary' is nil, `ispell-complete-word-dict' is used
-  ;;  but I prefer hard code the dictionary path. That's more portable.
-  (setq company-ispell-dictionary (file-truename "~/.emacs.d/misc/english-words.txt"))
-  ;; (setq company-ispell-dictionary (file-truename "~/.emacs.d/misc/apsell-en-sorted.txt"))
-  )
+;;   ;; OPTIONAL, if `company-ispell-dictionary' is nil, `ispell-complete-word-dict' is used
+;;   ;;  but I prefer hard code the dictionary path. That's more portable.
+;;   (setq company-ispell-dictionary (file-truename "~/.emacs.d/misc/english-words.txt"))
+;;   ;; (setq company-ispell-dictionary (file-truename "~/.emacs.d/misc/apsell-en-sorted.txt"))
+;;   )
 
-(add-hook 'text-mode-hook 'text-mode-hook-setup)
+;; (add-hook 'text-mode-hook 'text-mode-hook-setup)
 
 ;; https://endlessparentheses.com/ispell-and-org-mode.html
 (defun endless/org-ispell ()
