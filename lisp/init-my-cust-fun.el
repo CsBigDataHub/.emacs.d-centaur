d9c0423c30cd3f2aa84379847437b904fb146937
Chetan Koneru <chetan.koneru@optum.com>
2020-11-30 12:07:13 -0600

    Added insert-file name ivy-add-actions and functions

--- lisp/init-my-cust-fun.el
+++ lisp/init-my-cust-fun.el
@@ -3454,4 +3454,32 @@ are defining or executing a macro."
               load-elt (and loads (car loads)))))
     load-elt))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; insert file name at point                                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; https://www.emacswiki.org/emacs/InsertFileName
+(defun my/insert-file-name (filename &optional args)
+  "Insert name of file FILENAME into buffer after point.
+
+  Prefixed with \\[universal-argument], expand the file name to
+  its fully canocalized path.  See `expand-file-name'.
+
+  Prefixed with \\[negative-argument], use relative path to file
+  name from current directory, `default-directory'.  See
+  `file-relative-name'.
+
+  The default with no prefix is to insert the file name exactly as
+  it appears in the minibuffer prompt."
+  ;; Based on insert-file in Emacs -- ashawley 20080926
+  (interactive "*fInsert file name: \nP")
+  (cond ((eq '- args)
+         (insert (expand-file-name filename)))
+        ((not (null args))
+         (insert filename))
+        (t
+         (insert (file-relative-name filename)))))
+
+(bind-keys*
+ ("M-m i i" . my/insert-file-name))
+
 (provide 'init-my-cust-fun)
