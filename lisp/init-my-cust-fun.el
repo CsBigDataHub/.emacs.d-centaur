1d0972ceec7959841b1a7186a7407d09a8eefa3f
Chetan Koneru <chetan.koneru@optum.com>
2020-12-14 12:28:51 -0600

    Added https://emacsredux.com/blog/2013/07/24/highlight-comment-annotations/

--- lisp/init-my-cust-fun.el
+++ lisp/init-my-cust-fun.el
@@ -3377,6 +3377,17 @@ It sets the transient map to all functions of ALIST."
 
 ;;;
 
+;; https://emacsredux.com/blog/2013/07/24/highlight-comment-annotations/
+;;(defun my-font-lock-comment-annotations ()
+;;  "Highlight a bunch of well known comment annotations.
+;;
+;;This functions should be added to the hooks of major modes for programming."
+;;  (font-lock-add-keywords
+;;   nil '(("\\<\\(FIX\\(ME\\)?\\|TODO\\|OPTIMIZE\\|HACK\\|REFACTOR\\):"
+;;          1 font-lock-warning-face t))))
+;;
+;;(add-hook 'prog-mode-hook 'my-font-lock-comment-annotations)
+
 (defun my/quit ()
   "Quit in current context.
 
