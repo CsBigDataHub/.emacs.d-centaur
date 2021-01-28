3a1b48c01b4b17dc84bb85d7ca8c4a0bb952f2ef
Chetan Koneru <chetan.koneru@optum.com>
2021-01-28 13:40:49 -0600

    Added init-pass file

---
+++ lisp/init-pass.el
@@ -0,0 +1,12 @@
+;;init-pass.el --- Initialize password-store configurations.	-*- lexical-binding: t -*-
+
+
+
+(use-package password-generator)
+
+(use-package pass)
+
+(use-package password-store)
+
+
+(provide 'init-pass)
