b7342f8e9f328092314601b42efe9775ee607358
Chetan Koneru <chetan.koneru@optum.com>
2021-02-10 14:38:20 -0600

    Added few certs notes

--- lisp/init-pass.el
+++ lisp/init-pass.el
@@ -1,5 +1,6 @@
 ;;init-pass.el --- Initialize password-store configurations.	-*- lexical-binding: t -*-
 
+;; https://www.masteringemacs.org/article/keeping-secrets-in-emacs-gnupg-auth-sources
 (require 'epa-file)
 (epa-file-enable)
 
