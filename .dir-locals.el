742966f0a377c8c58236310422c32f68791872c4
Chetan Koneru <chetan.koneru@optum.com>
2021-02-11 11:46:21 -0600

    update dir-locals variables

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((magit-status-mode (magit-todos-exclude-globs "*.elc" "elpa/**" "straight/**" "*.md" "*.org")))
+((magit-status-mode . ((eval . (remove-hook 'magit-refs-sections-hook 'magit-insert-tags))
+                       (eval . (magit-disable-section-inserter 'magit-insert-tags-header))
+                       (magit-todos-exclude-globs . ("*.elc" "elpa/**" "straight/**" "*.md" "*.org")))))
