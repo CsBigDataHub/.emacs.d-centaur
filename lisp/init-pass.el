5cf4fff02a706b3d730bee3af90d847106c8fd95
Chetan Koneru <chetan.koneru@optum.com>
2022-02-23 20:37:29 -0600

    updated pass to start up optimizations

--- lisp/init-pass.el
+++ lisp/init-pass.el
@@ -1,8 +1,26 @@
 ;;init-pass.el --- Initialize password-store configurations.	-*- lexical-binding: t -*-
 
 ;; https://www.masteringemacs.org/article/keeping-secrets-in-emacs-gnupg-auth-sources
-(require 'epa-file)
-(epa-file-enable)
+;; (require 'epa-file)
+;; (epa-file-enable)
+;; Enable encryption/decryption of .gpg files
+(use-package epa-file
+  :ensure nil
+  :defer t
+  :after epg
+  :commands epa-file-enable
+  :config  (progn
+             ;; Unfortunately there is bug in gpg which disabled this
+             ;; ~/.gnupg/gpg-agent.conf should contain:
+             ;; allow-emacs-pinentry
+             ;; allow-loopback-pinentry
+             ;; Restart with:
+             ;; gpgconf --reload gpg-agent
+             ;; (setq epa-pinentry-mode 'loopback)
+
+             (setq epa-file-name-regexp "\\.\\(gpg\\|asc\\)$")
+             (epa-file-name-regexp-update))
+  :hook (after-init . epa-file-enable))
 
 (use-package password-generator)
 
