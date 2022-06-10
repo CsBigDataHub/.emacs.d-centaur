749733898635fd56b8dc5db8b3124d94815242a7
Vincent Zhang <seagle0128@gmail.com>
2022-06-10 12:00:09 +0800

    [Performance] Optimize performance.

--- early-init.el
+++ early-init.el
@@ -40,6 +40,8 @@
 ;; initialization, so we must prevent Emacs from doing it early!
 (setq package-enable-at-startup nil)
 
+(setq load-prefer-newer noninteractive)
+
 ;; Inhibit resizing frame
 (setq frame-inhibit-implied-resize t)
 
